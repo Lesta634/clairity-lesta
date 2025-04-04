a3a7dce022db45123c7f4266ac50b43b47ea7456
Lesta <lestaeliud@gmail.com>
2025-04-04 04:44:57 +0300

    Update ownerConfig.js

--- media/settings/ownerConfig.js
+++ media/settings/ownerConfig.js
@@ -2,9 +2,9 @@
 const fs = require('fs')
 // gausah di ubah rek karna
 // udh ada request pas di scan pairing
-global.ownername = 'xZiyy';
-global.owner = '6283159451487';
-global.botname = 'clairity bot';
+global.ownername = 'EliudLesta';
+global.owner = '254725100848';
+global.botname = 'New LESTA bot';
 
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
